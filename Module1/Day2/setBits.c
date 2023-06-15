58d655032cb5bb63c22d3922e48aed07e45535f2
vamsi <vamsi@vamsi.vamsi>
2023-06-15 11:13:27 +0530

    commit : Day2 : setBits.c modified

--- Module1/Day2/setBits.c
+++ Module1/Day2/setBits.c
@@ -1,4 +1,41 @@
 #include<stdio.h>
+/*     let x = 9
+       so  x = 1001
+       count=0
+       iteration 1 : 
+       		     x & 1 = 1001
+       		     	     0001
+       		     	     ----
+       		     	     	1
+       increment count, count = 0
+       x >> 1     then   x = 0100
+       
+       iteration 2:
+       		     x & 1  = 0100
+       		             0001
+       		             ----
+       		             	0
+       x >> 1    then   x =  0010
+       
+       iteration 3:
+       		     x & 1  = 0010
+       		              0001
+       		              ----
+       		                 0
+       x >> 1    then   x = 0001
+       
+       iteration 4:
+                     x & 1 =  0001
+                              0001
+                              ----
+                                 1
+       increment count, then count = 2
+       x >> 1 then x = 0000
+       
+       x = 0, stop
+       			
+       
+*/
 int noOfSetBits(int x)
 {
 	int count = 0;
