4cb2d1b4a530688d643f9bf2f0c5d0b695a0d75e
vamsi <vamsi@vamsi.vamsi>
2023-06-15 09:25:33 +0530

    commit : modified bitOperations.c

--- Module1/Day1/bitOperations.c
+++ Module1/Day1/bitOperations.c
@@ -5,7 +5,7 @@ int bit_operations(int num, int oper_type) {
             num |= (1 << 0);   // Set 1st bit
             break;
         case 2:
-            num &= ~(1 << 31);  // Clear 31st bit
+            num &= ~(1 << 30);  // Clear 31st bit
             break;
         case 3:
             num ^= (1 << 15);  // Toggle 16th bit
